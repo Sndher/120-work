e5cb1827dcb7a7820db170a635bf87bbbf3b9572
Shelby Decker <sndher@gmail.com>
2018-05-10 12:24:51 -0600

    .

--- hw-13/sketch.js
+++ hw-13/sketch.js
@@ -1,24 +1,24 @@
-function setup(){
-  createCanvas(600,600);
-}
-function draw(){
-  background(10,50,100);
-}
+//function setup(){
+//  createCanvas(600,600);
+//}
+//function draw(){
+//  background(10,50,100);
+//}
 
-//import processing.sound.*;
-//SoundFile file;
+import processing.sound.*;
+SoundFile file;
 
-//void setup() {
-//size(640, 360);
-//background(255);
+void setup() {
+size(640, 360);
+background(255);
 
-// Load a soundfile from the /data folder of the sketch and play it back
-  //  file = new SoundFile(this, "mountain.mp3");
-  //    file.play();
-  //}
+ Load a soundfile from the /data folder of the sketch and play it back
+    file = new SoundFile(this, "mountain.mp3");
+      file.play();
+  }
 
-//void draw() {
-//}import p
+void draw() {
+}import p
 
 
 ///////////////////////////////////////////////////////////////////////
