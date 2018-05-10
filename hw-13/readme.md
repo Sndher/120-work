42465d75e6bcc98ad0eba086993e2b668f9f1927
Shelby Decker <sndher@gmail.com>
2018-05-10 17:25:35 -0600

    edits

--- hw-13/readme.md
+++ hw-13/readme.md
@@ -1,3 +1,9 @@
-# Empty p5 sketch.
+## Week 13
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+#Final Project
+
+####Objective
+For my final project my goal was to work with sound and create an equalizer or music analyzer. I wanted to create a sketch that would change with the beats of music.
+
+####Study
+I watched The Coding Train sound tutorial videos for references and guides. I also read chapter 13 in Make: Getting Started with p5.js by McCarthy, Reas & Fry. Read online troubleshooting guides in regard to setting up a local server. Studied FFT and frequency components.
--- hw-13/sketch.js
+++ hw-13/sketch.js
@@ -1,3 +1,109 @@
+
+var img;
+var snowflakeImage
+var flakes = []; //declaring the array
+let pos_y = 20
+
+/////////////////////////////////////////
+
+function preload(){
+  img = loadImage("trees.jpg");
+}
+/////////////////////////////////////////
+
+function setup(){
+  createCanvas(850,600);
+
+  //var numSnowflakes=30
+  //for (var i = 0; i < numSnowflakes; i++){
+  //  var x = random(-40, width-40);
+  //  var y = map(i, 0, numSnowflakes, -100, -200);
+  //  flakes[i] = new Flake(snowflakeImage, x, y);
+  //}
+}
+/////////////////////////////////////////
+
+function draw(){
+image(img, 0,0);
+
+for (var i=0; i<flakes.length; i++){
+  flakes[i].update();
+  flakes[i].display();
+
+}
+
+  snowflake( 50, pos_y, -50);
+
+  // update the ellipses position
+  pos_y = pos_y + 1.5;
+  // bound it to the width of the canvas
+  pos_y = pos_y % height;
+}
+/////////////////////////////////////////
+
+function snowflake(x,y){
+  beginShape();
+translate(-100,-100);
+  push();
+  translate(x,y);
+  strokeWeight(7);
+  strokeCap(ROUND);
+  line(100,100,200,200);
+    strokeWeight(7);
+    strokeCap(ROUND);
+    line(100,200,200,100);
+      strokeWeight(7);
+      strokeCap(ROUND);
+      line(150,215,150,85);
+        strokeWeight(7);
+        strokeCap(ROUND);
+        line(85,150,215,150);
+        pop();
+  endShape();
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+
+// SOUND CODE BELOW
+      // NOTE // did not work due to local server
+
+////////////////////////////////////////////////////////////////////
+// var music;
+// var radius=120;
+// var x=0;
+// var speed = 1.0;
+// var direction=1;
+
+// function preload(){
+  // music = loadSound("audio/piano.mp3");
+// }
+//function setup(){
+  // createCanvas(600,600);
+  // ellipseMode(RADIUS);
+  // x=width/2; //starts in center
+// }
+// function draw(){
+  // background(0);
+  // x+=speed*direction;
+  // fit((x>width-radius)||(x<radius)) {
+    // direction= -direction; //flip direction
+    // music.play();
+  // }
+  // if (direction ==1) {
+    // arc(x,220, radius, radius, 0.52, 5.76); //face right
+  // }
+  // else{
+    // arc(x,220, radius, radius, 3.67, 8.9); //face left
+  // }
+// }
+
+
+////////////////////////////////////////////////////////////////////
+
 //function setup(){
 //  createCanvas(600,600);
 //}
@@ -22,64 +128,65 @@
 
 
 ///////////////////////////////////////////////////////////////////////
-var soundtrack;
-var playbutton, stopbutton;
-var analyzer;
+//var soundtrack;
+//var playbutton, stopbutton;
+//var analyzer;
+//var uploadLoading = false;
 
-function preload()
-{
-  soundtrack = loadSound('mountain.mp3');
-}
+//function preload()
+//{
+//  soundtrack = loadSound('audio/mountain.mp3');
+//}
 
-function setup()
-{
-  createCanvas(600, 600);
+//function setup()
+//{
+//  createCanvas(600, 600);
 
-   loop sound
-  soundtrack.loop();
+//   loop sound
+//  soundtrack.loop();
 
-   stop sound to prevent it from playing automatically
-  soundtrack.stop();
+//   stop sound to prevent it from playing automatically
+//  soundtrack.stop();
 
-   play button
-  playbutton = createButton('Play');
-  playbutton.position(25, 25);
-  playbutton.mousePressed(playsound);
+//   //play button
+//  playbutton = createButton('Play');
+//  playbutton.position(25, 25);
+//  playbutton.mousePressed(playsound);
 
-   stop button
-  stopbutton = createButton('Stop');
-  stopbutton.position(75, 25);
-  stopbutton.mousePressed(stopsound);
+//   stop button
+//  stopbutton = createButton('Stop');
+//  stopbutton.position(75, 25);
+//  stopbutton.mousePressed(stopsound);
 
-   music visualizer
-  analyzer = new p5.Amplitude();
-  analyzer.setInput(soundtrack);
-}
+//   music visualizer
+//  analyzer = new p5.Amplitude();
+//  analyzer.setInput(soundtrack);
+//}
 
-function draw()
-{
-  background(255);
+//function draw()
+//{
+//  background(255);
 
-   draw an ellipse based on current volume level
-  var vol = analyzer.getLevel();
-  Stroke(3);
-  fill("white");
-  ellipse(width/2, height/2, map(vol, 0, 1, 0, width), map(vol, 0, 1, 0, height));
+//   draw an ellipse based on current volume level
+//  var vol = analyzer.getLevel();
+//  Stroke(3);
+//  fill("white");
+//  ellipse(width/2, height/2, map(vol, 0, 1, 0, width), map(vol, 0, 1, 0, height));
 
-}
+//}
 
-function playsound()
-{
-  if(soundtrack.isPlaying() == false)
-  {
-    soundtrack.play();
-  }
-}
+//function playsound()
+//{
+//  if(soundtrack.isPlaying() == false)
+//  {
+//    soundtrack.play();
+//  }
+//}
 
-function stopsound()
-{
-  if(soundtrack.isPlaying() == true)
-  {
-    soundtrack.pause();
-  }
-}
+//function stopsound()
+//{
+//  if(soundtrack.isPlaying() == true)
+//  {
+//    soundtrack.pause();
+//  }
+//}
